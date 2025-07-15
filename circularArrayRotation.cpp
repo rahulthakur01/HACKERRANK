9953acdde1e1b8e9b96f863577c69a03a5a0fe47
Rahul Kumar <rahul.th7632@gmail.com>
2025-07-15 17:44:41 +0530

    updated

--- circularArrayRotation.cpp
+++ circularArrayRotation.cpp
@@ -1,13 +1,31 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+vector<int> circularArrayRotation(vector<int> a, int k, vector<int> queries) {
+        int n = a.size();
+        k = k %n;
+        vector<int> ans;
+    for(int i = 0; i<queries.size(); i++){
+        int queryIndex = queries[i];
+        int originalIndex = (queryIndex - k + n) %n;
+        ans.push_back(a[originalIndex]);
+    }
+    return ans;
+}
+
 
 int main(){
     int n = 3;
     int k = 2;
     int q = 3;
-    // vector<int> arr(n);
+  
     vector<int> arr = {1,2,3};
     vector<int> queries = {0,1,2};
+    vector<int> result = circularArrayRotation(arr, k, queries);
+    for(int value: result){
+        cout<< value << endl;
+    }
+    return 0;
 }
 // #include <iostream>
 // #include <vector>
