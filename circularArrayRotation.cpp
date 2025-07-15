54b54f26e56d5abc773c271bc1ec00025d2e4d66
rahulthakur01 <abhisharma7091@gmail.com>
2025-07-15 08:38:12 -0700

    updated

--- circularArrayRotation.cpp
+++ circularArrayRotation.cpp
@@ -4,11 +4,15 @@ using namespace std;
 vector<int> circularArrayRotation(vector<int> a, int k, vector<int> queries) {
         int n = a.size();
         k = k %n;
+        vector<int> roated(n);
+        // first rotate array
+        for(int i = 0; i<n; i++){
+            roated[(i + k) % n] = a[i];
+        }
+        // then store and print
         vector<int> ans;
     for(int i = 0; i<queries.size(); i++){
-        int queryIndex = queries[i];
-        int originalIndex = (queryIndex - k + n) %n;
-        ans.push_back(a[originalIndex]);
+        ans.push_back(roated[queries[i]]);
     }
     return ans;
 }
@@ -17,7 +21,6 @@ vector<int> circularArrayRotation(vector<int> a, int k, vector<int> queries) {
 int main(){
     int n = 3;
     int k = 2;
-    int q = 3;
   
     vector<int> arr = {1,2,3};
     vector<int> queries = {0,1,2};
@@ -27,32 +30,4 @@ int main(){
     }
     return 0;
 }
-// #include <iostream>
-// #include <vector>
-// using namespace std;
 
-// int main() {
-//     int n, k, q;
-//     cin >> n >> k >> q;
-
-//     vector<int> a(n);
-//     for (int i = 0; i < n; i++) {
-//         cin >> a[i];
-//     }
-
-//     // Handle cases where k >= n
-//     k = k % n;
-
-//     vector<int> queries(q);
-//     for (int i = 0; i < q; i++) {
-//         cin >> queries[i];
-//     }
-
-//     for (int i = 0; i < q; i++) {
-//         int queryIndex = queries[i];
-//         int originalIndex = (queryIndex - k + n) % n;
-//         cout << a[originalIndex] << endl;
-//     }
-
-//     return 0;
-// }
