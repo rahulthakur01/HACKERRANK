628be2586b03055707c6412531a46557b8ca4a43
rahulthakur01 <abhisharma7091@gmail.com>
2025-07-15 20:19:03 -0700

    updated

--- jumpingOntheCloud.cpp
+++ jumpingOntheCloud.cpp
@@ -5,15 +5,22 @@ using namespace std;
 int jumpingOnCloud(vector<int> &c, int k){
     int energy = 100;
     int n = c.size();
-    int i = 0;
-    while(true){
-
+    int position;
+    for(int i = 0; i<n; i=i+2){
+        position = (i + k)%n;
+        if(c[position] == 1){
+            energy = energy-3;
+        }else{
+            energy = energy -1;
+        }
     }
+    return energy;
 }
 
 int main(){
     int k = 2;
     vector<int> arr = {0, 0, 1, 0, 0, 1, 1, 0};
-    int result = jumpingOnCloud(k, arr);
+    int result = jumpingOnCloud(arr, k);
+    cout<< result << endl;
     return 0;
 }
