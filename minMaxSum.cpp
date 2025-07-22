2aabc8fdceda528ae56974b895c50233f4eaf544
Rahul Kumar <rahul.th7632@gmail.com>
2025-07-22 10:30:14 +0530

    mixMax updated

--- minMaxSum.cpp
+++ minMaxSum.cpp
@@ -1,35 +1,31 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<climits>
+#include<vector>
 using namespace std;
 
-void minMaxSum(vector<int> arr)
-{
-    long totalSum = 0;
-    long minValue = arr[0];
-    long maxValue = arr[0];
 
-    for (int i = 0; i < arr.size(); i++)
-    {
+void minMax(vector<int> &arr){
+    long totalSum = 0;
+    long maxValue = INT_MIN;
+    long minValue = INT_MAX;
+
+    for(int i = 0; i< arr.size(); i++){
         totalSum = totalSum + arr[i];
-        if (arr[i] > minValue)
-        {
-            minValue = arr[i];
+        if(arr[i] <minValue){
+            minValue = arr[i] ;
         }
-        if(maxValue > arr[i])
-        {
+        if(maxValue < arr[i]){
             maxValue = arr[i];
         }
     }
-    long minSum = totalSum - minValue;
-    long maxSum = totalSum - maxValue;
-    cout << minSum << " " << maxSum << endl;
+    long minSum = totalSum - maxValue;
+    long maxSum = totalSum - minValue;
+
+    cout<< minSum << " " << maxSum << endl;
 }
 
-int main()
-{
-    vector<int> arr = {1, 5, 7, 9};
-
-    minMaxSum(arr);
-
+int main(){
+    vector<int> arr =  {1, 5, 7, 9};
+    minMax(arr);
     return 0;
 }
